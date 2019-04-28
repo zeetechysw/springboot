14b05d21f00c019dc9cc33f59cde2d423f214b1f
zeetechysw <40381184+zeetechysw@users.noreply.github.com>
2019-04-28 15:54:52 +0800

    GitTest2

--- src/main/java/com/example/demo/action/HelloAction.java
+++ src/main/java/com/example/demo/action/HelloAction.java
@@ -3,6 +3,8 @@ package com.example.demo.action;
 import com.example.demo.entity.ZtArea;
 import com.example.demo.repository.AreaRepository;
 import com.example.demo.service.ManageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,27 +14,34 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/hello")
-@CrossOrigin(origins = "*",maxAge = 3600)
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class HelloAction {
+    Logger logger = LoggerFactory.getLogger(getClass());
     @Autowired
     public AreaRepository areaRepository;
     @Autowired
     public ManageService manageService;
+
     @RequestMapping("/test")
     @ResponseBody
-    public List test(String name, Model model){
-        List list=manageService.getAreaList(name);
-        model.addAttribute("aress",list);
+    public List test(String name, Model model) {
+        logger.info("获得全部");
+        List list = manageService.getAreaList(name);
+        model.addAttribute("aress", list);
         return list;
     }
+
     @RequestMapping("/getOneArea/{id}")
     @ResponseBody
-    public ZtArea getOneArea(@PathVariable("id") Integer id){
+    public ZtArea getOneArea(@PathVariable("id") Integer id) {
+        logger.warn("获得单个");
         return areaRepository.getOne(id);
     }
+
     @RequestMapping("/saveOne")
     @ResponseBody
-    public ZtArea saveOne(@RequestBody ZtArea area){
+    public ZtArea saveOne(@RequestBody ZtArea area) {
+        logger.error("保存");
         return areaRepository.save(area);
     }
 }
